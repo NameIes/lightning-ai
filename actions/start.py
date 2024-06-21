bd26e5f2a0604104cee931b0486ea6244210a560
Nameless <nameies@yandex.ru>
2024-06-21 15:01:57 +0300

    Added YOLO to start action. For now without trained model.

--- actions/start.py
+++ actions/start.py
@@ -5,8 +5,22 @@ from pynput import keyboard
 from mss import mss
 from threading import Thread
 from colorama import Fore, Style
+from ultralytics import YOLO
 from utils.window_box import get_rect_by_name
 from core.storage import Storage
+
+
+def place_results(results, resized):
+    for result in results:
+        for box in result.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            conf = box.conf[0]
+            cls = box.cls[0]
+
+            cv2.rectangle(resized, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+            label = f'{cls}: {conf:.2f}'
+            cv2.putText(resized, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 
 def start():
@@ -29,13 +43,19 @@ def start():
 
     Storage().set_data('lightning_started', True)
     while Storage().data['lightning_started']:
-        box = get_rect_by_name('Code.exe')
+        box = get_rect_by_name('firefox.exe')
         sct_img = sct.grab(box)
-
         img = np.array(sct_img)
-        res = cv2.resize(img, (960, 540), interpolation=cv2.INTER_CUBIC)
+        if img.shape[2] == 4:
+            img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+        resized = cv2.resize(img, (960, 544), interpolation=cv2.INTER_CUBIC)
 
-        cv2.imshow('screen', res)
+        model = YOLO('./models/pretraining.pt')
+        results = model(resized, imgsz=(960, 544))[0]
+
+        place_results(results, resized)
+
+        cv2.imshow('screen', resized)
         cv2.waitKey(1)
 
     cv2.destroyAllWindows()
