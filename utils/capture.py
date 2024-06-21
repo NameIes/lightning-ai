ad714ef66767cf7bd4c9e6dbf27bbde45139457a
Nameless <nameies@yandex.ru>
2024-06-21 17:03:52 +0300

    Simple optimization of screenshot correction.

--- utils/capture.py
+++ utils/capture.py
@@ -8,8 +8,6 @@ def take_screenshot(sct: MSSBase, process_name: str) -> cv2.typing.MatLike:
     box = get_rect_by_name(process_name)
     sct_img = sct.grab(box)
     img = np.array(sct_img)
-    if img.shape[2] == 4:
-        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
     return img
 
@@ -23,5 +21,7 @@ def resize_image(img: cv2.typing.MatLike) -> tuple[cv2.typing.MatLike, tuple, tu
         new_height = 640
         new_width = int((640 / height) * width)
     resized_image = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+    if resized_image.shape[2] == 4:
+        resized_image = cv2.cvtColor(resized_image, cv2.COLOR_BGRA2BGR)
 
     return resized_image, (width, height), (new_width, new_height)
