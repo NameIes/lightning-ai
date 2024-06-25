ecd85098be4a7b908afb04281cc0f6278829d8b2
Nameless <nameies@yandex.ru>
2024-06-25 14:08:57 +0300

    Fixed old storage calls.

--- core/lightning.py
+++ core/lightning.py
@@ -24,11 +24,11 @@ class Lightning:
     def get_heads(self, boxes) -> list[Rect]:
         rects = []
         class_id = []
-        if self._st.data['team'] == 'all':
+        if self._st['team'] == 'all':
             class_id = [2, 4]
-        elif self._st.data['team'] == 'ct':
+        elif self._st['team'] == 'ct':
             class_id = [4]
-        elif self._st.data['team'] == 't':
+        elif self._st['team'] == 't':
             class_id = [2]
 
         for box in boxes:
@@ -40,11 +40,11 @@ class Lightning:
     def get_bodies(self, boxes) -> list[Rect]:
         rects = []
         class_id = []
-        if self._st.data['team'] == 'all':
+        if self._st['team'] == 'all':
             class_id = [1, 3]
-        elif self._st.data['team'] == 'ct':
+        elif self._st['team'] == 'ct':
             class_id = [1]
-        elif self._st.data['team'] == 't':
+        elif self._st['team'] == 't':
             class_id = [3]
 
         for box in boxes:
@@ -54,11 +54,11 @@ class Lightning:
         return rects
 
     def aimbot(self, boxes):
-        if self._st.data['aimbot_priority'] == 'Head':
+        if self._st['aimbot_priority'] == 'Head':
             heads = self.get_heads(boxes)
             if len(heads) > 0:
                 boxes = heads
-        if self._st.data['aimbot_priority'] == 'Body':
+        if self._st['aimbot_priority'] == 'Body':
             bodies = self.get_bodies(boxes)
             if len(bodies) > 0:
                 boxes = bodies
@@ -67,7 +67,7 @@ class Lightning:
         near_rect = min(boxes, key=lambda x: self.vector_length(cur[0], cur[1], x.get_center()[0], x.get_center()[1]))
 
         distance = self.vector_length(cur[0], cur[1], near_rect.get_center()[0], near_rect.get_center()[1])
-        if int(distance) > self._st.data['aim_max_distance']:
+        if int(distance) > self._st['aim_max_distance']:
             return
 
         x_shift = near_rect.get_center()[0] - cur[0]
@@ -78,18 +78,18 @@ class Lightning:
         cur = (self.screen_size[0] // 2, self.screen_size[1] // 2)
         for box in boxes:
             if self.is_cursor_in_rect(cur, box):
-                if self._st.data['aim_delay'] > 0:
-                    sleep(self._st.data['aim_delay'] / 1000)
+                if self._st['aim_delay'] > 0:
+                    sleep(self._st['aim_delay'] / 1000)
                 self._mouse.click()
 
     def inuria(self, boxes):
         self.aimbot(boxes)
 
     def process(self, boxes) -> None:
-        if self._st.data['team'] == 'off':
+        if self._st['team'] == 'off':
             return
 
-        aim_type = self._st.data['aim_type']
+        aim_type = self._st['aim_type']
 
         if aim_type == 'aimbot':
             self.aimbot(boxes)
