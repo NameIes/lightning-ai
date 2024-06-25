3edb8d0d17a7271dc9f717ad1fcf8ebec85151b0
Nameless <nameies@yandex.ru>
2024-06-25 13:51:59 +0300

    Added main logic cycle.

--- actions/start.py
+++ actions/start.py
@@ -32,13 +32,20 @@ def start() -> None:
     gsi_server = GSIServerManager(('localhost', 8003), 'MYTOKENHERE')
     gsi_server.start()
 
-    detection = YOLODetection((1280, 1280), 'cs2.exe')
+    detection = YOLODetection((1280, 1280), data_storage['settings']['process_name'])
     lightning = Lightning(mouse, detection.screensize)
     mouse.start(lightning)
 
     while data_storage['lightning_started']:
-        pass
+        orig_img = detection.take_screenshot()
+        resized_img, orig_size = detection.resize_image(orig_img)
+        transformed_boxes = detection.predict(resized_img, orig_size)
+        lightning.process(transformed_boxes)
+
+        if data_storage['settings']['show_yolo']:
+            detection.show_results(transformed_boxes)
 
     keyboard.stop()
     mouse.stop()
     gsi_server.stop()
+    detection.stop()
