e3c916ef0520d9559b6eefab8d6c11e838b27aee
Nameless <nameies@yandex.ru>
2024-06-25 13:47:22 +0300

    Now possible to set mouse on_click function.

--- core/mouse.py
+++ core/mouse.py
@@ -6,20 +6,22 @@ class MouseListener:
     def __init__(self) -> None:
         self._thread = None
         self._lightning = None
+        self._click_function = lambda: None
 
     def on_click(self, x, y, button, pressed) -> None:
         if button == mouse.Button.left and not pressed:
             print('Left btn pressed')
+            self._click_function()
 
     def start(self, lightning) -> None:
         self._lightning = lightning
         self.thread = mouse.Listener(on_click=self.on_click)
         self.thread.start()
 
-    def move_mouse(x, y):
+    def move(x, y):
         win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0)
 
-    def mouse_click():
+    def click():
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0,0,0)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0,0,0)
 
